0ee89fe7d7550305cd5ebe67d1a8755674d4d4d3
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-08 16:42:27 +0300

    Вторая домашка с переменными

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         long number4 = 123456789L;
         float number5 = 3.5f;
         double number6 = 3.14;
-        System.out.println("Значение переменной number1 с    типом int равно " + number1);
+        System.out.println("Значение переменной number1 с типом int равно " + number1);
         System.out.println("Значение переменной number2 с типом byte равно " + number2);
         System.out.println("Значение переменной number3 с типом short равно " + number3);
         System.out.println("Значение переменной number4 с типом long равно " + number4);
@@ -50,7 +50,26 @@ public class Main {
         int totalWhitePots = totalOffices * whitePots;
         int totalBrownPots = totalOffices * brownPots;
         System.out.println("В школе, где " + totalOffices + " классов, нужно " + totalWhitePots + " банок белой краски и " + totalBrownPots + " банок коричневой краски");
-
+        //Задача №6
+        int weightBanan = 80;
+        int weightMilk = 105;
+        int weightPlombir = 100;
+        int weightEggs = 70;
+        int grPerKg = 1000;
+        int weightBreakfastGram = (5 * weightBanan) + (2 * weightMilk) + (2 * weightPlombir) + (4 * weightEggs);
+        float weightBreakfastKilo = (float) ((5 * weightBanan) + (2 * weightMilk) + (2 * weightPlombir) + (4 * weightEggs)) / grPerKg;
+        System.out.println("Вес спортивного завтрака " + weightBreakfastGram + " в граммах");
+        System.out.println("Вес спортивного завтрака " + weightBreakfastKilo + " в килограммах");
+        //Задача №7
+        int totalWeightGram = 7000;
+        int weightProgram1 = 250;
+        int weightProgram2 = 500;
+        int daysProgram1 = totalWeightGram / weightProgram1;
+        int daysProgram2 = totalWeightGram / weightProgram2;
+        int daysAverage = (daysProgram1 + daysProgram2) / 2;
+        System.out.println("Количество дней на похудение, если терять в весе по 250 грамм в день - " + daysProgram1 + " дней");
+        System.out.println("Количество дней на похудение, если терять в весе по 500 грамм в день - " + daysProgram2 + " дней");
+        System.out.println("Количество дней на похудение в среднем - " + daysAverage + " дней");
 
 
     }
